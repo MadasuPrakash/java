580eee0afe1c4a01f35411375ed36f370de452fb
Prakash_Madasu <madasuprakash143@gmail.com>
2024-11-07 21:32:21 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+class concat_str
+{public static void main(String args[]){
+String s1="abc";
+String s2="pqr";
+String s3=s1.concat(s2);
+System.out.println(s3);
+boolean s4=s1.equals(s2);
+System.out.println(s4);}}
