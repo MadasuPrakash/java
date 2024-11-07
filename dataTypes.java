ced11a1a04072a39ea681627349afc7add599ab7
Prakash_Madasu <madasuprakash143@gmail.com>
2024-11-07 21:31:42 +0530

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,16 @@
+class datatypes_ex
+{
+static int var1;
+float hightemp,lowtemp;
+static double var3;
+static long var4;
+static boolean  var5;
+public static void main(String args[]){
+System.out.println("var1="+var1);
+System.out.println(""+hightemp,lowtemp);
+System.out.println("var3="+var3);
+System.out.println("var4="+var4);
+System.out.println("var5="+var5);
+
+}
+}
