d7715213ef7b50f0da9825bcc1900f2f6559ebbb
Prakash_Madasu <madasuprakash143@gmail.com>
2024-11-07 21:32:58 +0530

    Create StringTokenizer.java

---
+++ StringTokenizer.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+import java.dutil.stringTokenizer;
+class S_tokenizer{
+public static void main(String args[]){
+stringTokenizer st=new stringTokenizer("this is java world","");
+while(st.hasMoreTokens()){
+System.out.println(st.nextToken());}
+}
+}
